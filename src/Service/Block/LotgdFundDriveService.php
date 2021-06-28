30c78c87c4392e6c3a08baf9928133fcca1bd47c
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-06-28 16:53:05 +0200

    First commit

---
+++ src/Service/Block/LotgdFundDriveService.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This file is part of "LoTGD Bundle Fund Drive".
+ *
+ * @see https://github.com/lotgd-core/bundle-fund-drive
+ *
+ * @license https://github.com/lotgd-core/bundle-fund-drive/blob/main/LICENSE
+ * @author IDMarinas
+ *
+ * @since 0.1.0
+ */
+
+namespace Lotgd\Bundle\FunDrive\Service\Block;
+
+use Sonata\BlockBundle\Model\Block;
+use Sonata\BlockBundle\Event\BlockEvent;
+
+class LotgdFundDriveService
+{
+    public function onBlock(BlockEvent $event)
+    {
+        $block = new Block();
+        $block->setId(uniqid('', true));
+        $block->setSettings($event->getSettings());
+        $block->setType('lotgd_bundle.fund_drive.block.paypal');
+
+        $event->addBlock($block);
+    }
+}
