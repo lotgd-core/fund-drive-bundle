03a1c5d5a933f71b7e06b89b206fe7ca69d63540
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-07-03 14:53:32 +0200

    Fix namespace

--- src/Service/Block/LotgdFundDriveService.php
+++ src/Service/Block/LotgdFundDriveService.php
@@ -11,7 +11,7 @@
  * @since 0.1.0
  */
 
-namespace Lotgd\Bundle\FunDrive\Service\Block;
+namespace Lotgd\Bundle\FundDrive\Service\Block;
 
 use Sonata\BlockBundle\Model\Block;
 use Sonata\BlockBundle\Event\BlockEvent;
